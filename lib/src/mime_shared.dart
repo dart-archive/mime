700621b4599b9677cc1b65cd0cf024ad8f4c6c3a
Mike Fairhurst <mfairhurst@google.com>
2020-09-22 13:18:49 -0700

    Remove unneeded dart:async import
    Since Dart 2.1, Future and Stream have been exported from dart:core.
    
    Alternatively, if for some reason this package needs to continue to
    support Dart 2.0, an exception can be made for this internally.

--- lib/src/mime_shared.dart
+++ lib/src/mime_shared.dart
@@ -3,8 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 library mime.shared;
 
-import 'dart:async';
-
 class MimeMultipartException implements Exception {
   final String message;
 
