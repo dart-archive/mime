c9e3e15900005182c5fdb3be5d2158f11593b0d6
matthias sweertvaegher <178714+mx1up@users.noreply.github.com>
2023-01-06 17:13:06 +0100

    improve lookup: case-insensitive and use firstWhereOrNull instead of for loop

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:collection/collection.dart';
+
 import 'default_extension_map.dart';
 
 /// Add an override for common extensions since different extensions may map
@@ -14,20 +16,17 @@ final Map<String, String> _preferredExtensionsMap = <String, String>{
 
 /// Lookup file extension by a given MIME type.
 /// If no extension is found, `null` is returned.
-String lookupExtension(String mimeType) {
-  if (_preferredExtensionsMap.containsKey(mimeType)) {
-    return _preferredExtensionsMap[mimeType];
+String? lookupExtension(String mimeType) {
+  final mimeTypeLC = mimeType.toLowerCase();
+  if (_preferredExtensionsMap.containsKey(mimeTypeLC)) {
+    return _preferredExtensionsMap[mimeTypeLC]!;
   }
-  String extension;
-  defaultExtensionMap.forEach((String ext, String test) {
-    if (mimeType.toLowerCase() == test) {
-      extension = ext;
-    }
-  });
-  return extension;
+  return defaultExtensionMap.entries
+      .firstWhereOrNull((entry) => entry.value == mimeTypeLC)?.key;
 }
 
-/// Allow for a user-specified MIME type-extension map that overrides the default.
+/// Allow for a user-specified MIME type-extension mapping that overrides the
+/// default.
 void addMimeType(String mimeType, String extension) {
-  _preferredExtensionsMap[mimeType] = extension;
+  _preferredExtensionsMap[mimeType.toLowerCase()] = extension.toLowerCase();
 }
