4b5b84eb393fcda8c7ee338311381b6bb1314deb
Dimitar Petrov <dpetrov787@gmail.com>
2023-04-05 18:47:51 +0100

    Add m4b mime type

--- lib/src/default_extension_map.dart
+++ lib/src/default_extension_map.dart
@@ -419,6 +419,7 @@ const Map<String, String> defaultExtensionMap = <String, String>{
   'm3u8': 'application/vnd.apple.mpegurl',
   // Source: https://www.rfc-editor.org/rfc/rfc4337#page-3
   'm4a': 'audio/mp4',
+  'm4b': 'audio/mp4',
   'm4u': 'video/vnd.mpegurl',
   'm4v': 'video/x-m4v',
   'ma': 'application/mathematica',
