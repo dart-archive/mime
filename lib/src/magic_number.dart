876a2c2b520ed03fa279e1baacbe9bf37d191702
Kevin Moore <kevmoo@google.com>
2023-07-18 09:51:40 -0700

    update lints, require Dart 3

--- lib/src/magic_number.dart
+++ lib/src/magic_number.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library mime.magic_number;
-
 class MagicNumber {
   final String mimeType;
   final List<int> numbers;
